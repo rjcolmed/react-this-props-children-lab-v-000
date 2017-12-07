cb08743fc16ee9678bed741fdd1d71853e125f73
[Ricardo Colon-Medina] <[rjcolmed@gmail.com]>
2017-12-07 17:37:49 -0500

    Done.

--- src/components/Invitation.js
+++ src/components/Invitation.js
@@ -1,1 +1,16 @@
-// Code Invitation Component Here
+import React from 'react';
+
+class Invitation extends React.Component {
+  render() {
+    return (
+      <div className="invitation">
+        <h1>You've been invited!</h1>
+        <div className="children">
+          {this.props.children}
+        </div>
+      </div>
+    )
+  }
+}
+
+export default Invitation;
--- src/components/ThemedDecorations.js
+++ src/components/ThemedDecorations.js
@@ -1,1 +1,17 @@
-// Code ThemedDecoration Component Here
+import React from 'react';
+
+class ThemedDecorations extends React.Component {
+  render() {
+    const themedChildren = React.Children.map(this.props.children, child => {
+      return (
+        <div className={this.props.theme}>{child}</div>
+      );
+    });
+
+    return (
+      <div>{themedChildren}</div>
+    );
+  }
+}
+
+export default ThemedDecorations;
